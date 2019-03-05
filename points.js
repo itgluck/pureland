bf6e15509e1a0e46c1789a7e53567a75d5853a4f
itgluck <it.gluck@ya.ru>
2019-03-06 00:08:03 +0200

    origin

---
+++ points.js
@@ -0,0 +1,29 @@
+var statesData = {
+    "type": "FeatureCollection", "features": [
+        {
+            "type": "Feature", "id": "1",
+            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/1.jpg","status": 5 },
+            "geometry": { "type": "Point", "coordinates": [20.511374, 54.710455] }
+        },
+        {
+            "type": "Feature", "id": "2",
+            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/2.jpg","status": 4 },
+            "geometry": { "type": "Point", "coordinates": [20.501053,54.713636] }
+        },
+        {
+            "type": "Feature", "id": "3",
+            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/3.jpg","status": 3 },
+            "geometry": { "type": "Point", "coordinates": [20.512404,54.715996] }
+        },
+        {
+            "type": "Feature", "id": "3",
+            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/1.jpg","status": 2 },
+            "geometry": { "type": "Point", "coordinates": [20.495280,54.720046] }
+        },
+        {
+            "type": "Feature", "id": "3",
+            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/3.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.486733,54.717032] }
+        },
+    ]
+};
