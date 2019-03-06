efa962ad9bc0a89f02b3f029d4777d569fd13444
itgluck <it.gluck@ya.ru>
2019-03-06 21:21:36 +0200

    last

--- points.js
+++ points.js
@@ -2,28 +2,68 @@ var statesData = {
     "type": "FeatureCollection", "features": [
         {
             "type": "Feature", "id": "1",
-            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/1.jpg","status": 5 },
-            "geometry": { "type": "Point", "coordinates": [20.511374, 54.710455] }
+            "properties": { 'date':"01.03.2019г","street": "ул. Госпитальная 34", 'img':"./img/35.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.511696,54.726137] }
         },
         {
             "type": "Feature", "id": "2",
-            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/2.jpg","status": 4 },
-            "geometry": { "type": "Point", "coordinates": [20.501053,54.713636] }
+            "properties": { 'date':"02.03.2019г","street": "ул. Теннистая Аллея 100", 'img':"./img/28.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.413153,54.724170] }
         },
         {
             "type": "Feature", "id": "3",
-            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/3.jpg","status": 3 },
-            "geometry": { "type": "Point", "coordinates": [20.512404,54.715996] }
+            "properties": { 'date':"02.03.2019г","street": "ул. Аллея Каштановая 1а", 'img':"./img/24.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.466161,54.712651] }
         },
         {
-            "type": "Feature", "id": "3",
-            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/1.jpg","status": 2 },
-            "geometry": { "type": "Point", "coordinates": [20.495280,54.720046] }
+            "type": "Feature", "id": "4",
+            "properties": { 'date':"02.03.2019г","street": "ул. Аллея Каштановая 1а", 'img':"./img/23.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.466322,54.712468] }
         },
         {
-            "type": "Feature", "id": "3",
-            "properties": { 'date':"05.03.2019г","street": "ул. Пушкина 36", 'img':"./img/3.jpg","status": 1 },
-            "geometry": { "type": "Point", "coordinates": [20.486733,54.717032] }
+            "type": "Feature", "id": "5",
+            "properties": { 'date':"02.03.2019г","street": "ул. Комсомольская 112", 'img':"./img/18.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.478388,54.735980] }
+        },
+        {
+            "type": "Feature", "id": "6",
+            "properties": { 'date':"03.03.2019г","street": "ул. Грибоедова 3", 'img':"./img/17.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.526587,54.733437] }
+        },
+        {
+            "type": "Feature", "id": "7",
+            "properties": { 'date':"03.03.2019г","street": "ул. Флоренского", 'img':"./img/14.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.441620,54.739954] }
+        },
+        {
+            "type": "Feature", "id": "8",
+            "properties": { 'date':"03.03.2019г","street": "ул. Флоренского", 'img':"./img/13.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.441158,54.739811] }
+        },
+        {
+            "type": "Feature", "id": "9",
+            "properties": { 'date':"04.03.2019г","street": "ул. Мусорского", 'img':"./img/11.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.502303,54.729030] }
+        },
+        {
+            "type": "Feature", "id": "10",
+            "properties": { 'date':"04.03.2019г","street": "ул. Захарова", 'img':"./img/10.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.471897,54.734655] }
+        },
+        {
+            "type": "Feature", "id": "11",
+            "properties": { 'date':"04.03.2019г","street": "ул. Железнодорожная 5", 'img':"./img/09.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.504913,54.691091] }
+        },
+        {
+            "type": "Feature", "id": "12",
+            "properties": { 'date':"04.03.2019г","street": "ул. Генерала-лейтенанта Озерова 41", 'img':"./img/08.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.507201,54.724991] }
+        },
+        {
+            "type": "Feature", "id": "13",
+            "properties": { 'date':"06.03.2019г","street": "ул. Дубовая аллея", 'img':"./img/06.jpg","status": 1 },
+            "geometry": { "type": "Point", "coordinates": [20.423825,54.718966] }
         },
     ]
 };
