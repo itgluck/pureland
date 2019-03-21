9ac9d14f8e567242738405c2f31d7b7f05196fd1
itgluck <it.gluck@ya.ru>
2019-03-21 23:09:08 +0200

    test

--- Leaflet.LocationShare.js
+++ Leaflet.LocationShare.js
@@ -15,7 +15,7 @@ var sendIcon = L.icon({
 })
 
 receiveIcon = L.icon({
-  iconUrl: "https://raw.githubusercontent.com/CliffCloud/Leaflet.LocationShare/master/dist/images/IconMapReceive.png",
+  iconUrl: "images/trash.png",
   iconSize:     [50, 50], // size of the icon
   iconAnchor:   [25, 50], // point of the icon which will correspond to marker's location
   popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
