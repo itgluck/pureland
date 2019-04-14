946be742a1deae7934ed0b89c5d80d4b56df029f
itgluck <it.gluck@ya.ru>
2019-04-14 09:03:10 +0200

    zoomtoclick

--- src/basic_structure.js
+++ src/basic_structure.js
@@ -1,4 +1,29 @@
-var map = L.map('map', { minZoom: 12 }).setView([54.6982, 20.505], 12);
+// Layers
+var points = L.layerGroup();
+var districts = L.layerGroup();
+// L.marker([54.90942,20.51657]).bindPopup('This is Littleton, CO.').addTo(points),
+// L.marker([54.20942,20.60657]).bindPopup('This is Denver, CO.').addTo(points);
+
+var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+
+var satellite = L.tileLayer(mbUrl, { id: 'mapbox.satellite'}),
+    streets = L.tileLayer(mbUrl, { id: 'mapbox.streets'});
+
+var map = L.map('map', { minZoom: 12, layers: [streets, points, districts] }).setView([54.6982, 20.505], 12);
+
+var baseLayers = {
+    "Спутник": satellite,
+    "Карта": streets
+};
+
+var overlays = {
+    "Городсие районы": districts,
+    "Проблемные зоны": points
+};
+
+L.control.layers(baseLayers, overlays, { position: "bottomright" }).addTo(map);
+
+
 
 var tile = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright" target="blank">OpenStreetMap</a>, &copy; <a href="http://cartodb.com/attributions" target="blank">CartoDB</a>' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -18,35 +43,20 @@ map.addControl(new L.Control.Search({
 
 
 
-// %%%%%%%%%%%%%%
-var points = L.layerGroup();
-var districts = L.layerGroup();
-// L.marker([54.90942,20.51657]).bindPopup('This is Littleton, CO.').addTo(points),
-// L.marker([54.20942,20.60657]).bindPopup('This is Denver, CO.').addTo(points);
 
-var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
 
-var satellite = L.tileLayer(mbUrl,  { id: 'mapbox.satellite', attribution: mbAttr }),
-    grayscale = L.tileLayer(mbUrl, { id: 'mapbox.light', attribution: mbAttr }),
-    streets = L.tileLayer(mbUrl, { id: 'mapbox.streets', attribution: mbAttr });
-
-var baseLayers = {
-    "Спутник": satellite,
-    "Grayscale": grayscale,
-    "День": streets
-};
-
-var overlays = {
-    "Проблемные зоны": points,
-    "Городсие районы": districts
-};
-
-L.control.layers(baseLayers, overlays, { position: "bottomright" }).addTo(map);
 // %%%%%%%%%%%%%%
 var info = L.control();
+var logo = L.control({ position: "topleft" });
+
+logo.onAdd = function(map) {
+    this.logodiv = L.DomUtil.create('div','logo');
+    this.logodiv.innerHTML = '<a href="mailto:pronin.s@i-labs.ru?subject=Чистая Страна - Калининград&body=Вы можете внести свой вклад в чистоту города, отправляйте фотографии мусорных контейнеров и свалок (укажите адрес и дату). Вместе мы сделаем город чище!По вопросам тел. 89673549307 Сергей Николаевич"><img src="images/logo_m.png" title="Сообщить о проблемной зоне"> </a>'
+    //  
+    
+    return this.logodiv;
+}
+logo.addTo(map);
 
 info.onAdd = function (map) {
     this._div = L.DomUtil.create('div', 'info width');
@@ -57,10 +67,10 @@ info.onAdd = function (map) {
 info.update = function (props) {
 
     this._div.innerHTML = (props ?
-        '<img src="' + props.img + '"><br>' +
+        ' <input type="checkbox" id="zoomCheck"><label for="zoomCheck"><img src="' + props.img + '"></label><br>' +
         'Адрес: <b>' + props.street +
         '</b><br>Дата: ' + props.date
-        : '<i>Обновлено: 13.04.19|11:30</i>');
+        : '<i>Обновлено: 14.04.19|08:30</i>');
 
 }
 
